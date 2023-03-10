2bb2b0e1d8806e20c3bd1c8cbf20f36030bb3b1e
noot <elizabethjbinks@gmail.com>
2023-03-10 18:00:07 -0500

    add msg.proto and protobuf gen build script

---
+++ build.rs
@@ -0,0 +1,11 @@
+use protobuf_codegen_pure;
+
+fn main() {
+    println!("cargo:rerun-if-changed=src/proto/msg.proto");
+    protobuf_codegen_pure::Codegen::new()
+        .out_dir("src/proto")
+        .inputs(&["src/proto/msg.proto"])
+        .include("src/proto")
+        .run()
+        .expect("Codegen failed.");
+}
