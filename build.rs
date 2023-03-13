a15297586c4ef759c5be08d2939e912d49f76dfe
noot <elizabethjbinks@gmail.com>
2023-03-13 15:54:26 -0400

    move proto/ out of src/, ignore build artifacts

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 fn main() {
-    println!("cargo:rerun-if-changed=src/proto/*.proto");
+    println!("cargo:rerun-if-changed=proto/");
     protobuf_codegen_pure::Codegen::new()
         .out_dir("src/proto")
-        .inputs(["src/proto/msg.proto", "src/proto/tx.proto"])
-        .include("src/proto")
+        .inputs(["proto/msg.proto", "proto/tx.proto"])
+        .include("proto")
         .run()
         .expect("Codegen failed.");
 }
